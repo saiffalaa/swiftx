3a8b18673828cf7422f33291b0f0b2c7dadc8305
Saif-Alaa-ElDin <saifalaa95@gmail.com>
2022-02-12 10:15:52 +0200

    setting the connection with graphQl server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import { gql, useQuery } from "@apollo/client";
 
 function App() {
+  const query = gql`
+    query {
+      categories {
+        name
+      }
+    }
+  `;
+  const { error, loading, data } = useQuery(query);
+  const [cate, setCate] = useState([]);
+  useEffect(() => {
+    const { categories } = data;
+    // console.log(categories);
+    setCate(categories);
+  }, [data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {cate.map((c) => (
+        <p>{c.name}</p>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
